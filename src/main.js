efdebcc031234ac42c69c136d07167770af085d6
张权 <18253626835@163.com>
2025-09-17 20:50:48 +0800

    删除多余的文件

--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import {createPinia} from "pinia";
 import router from "@/router/router.js";
 const pinia = createPinia();
 
-import '@/assets/extra-libs/datatables.net-bs4/css/dataTables.bootstrap4.css';
-import '@/assets/dist/css/style.min.css';
-
 
 createApp(App)
     .use(router)
